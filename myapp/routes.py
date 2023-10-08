768f03921eb35e05a8657af83fc1992fbcc1dace
Diego Morales <diegomorales1359@gmail.com>
2023-10-07 19:06:16 -0600

    Ruta para crear usuarios

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, redirect, url_for
-
+from flask import Blueprint, redirect, url_for, request, jsonify
 from .extensions import db
 from .models import User
 
@@ -13,6 +12,31 @@ def index():
 
 @main.route('/add/<username>')
 def add_user(username):
-    db.session.add(User(username=username))
+    db.session.add(User(username=username, password='test'))
     db.session.commit()
     return redirect(url_for("main.index"))
+
+@main.route('/createUser', methods=['POST'])
+def create_user():
+    try:
+        user_data = request.get_json()  # Obtener el objeto JSON de la solicitud
+
+        # Verificar si se recibieron los datos esperados
+        if 'email' in user_data and 'password' in user_data:
+            email = user_data['email']
+            password = user_data['password']
+
+            # Realizar alguna lógica con los datos recibidos
+            # Por ejemplo, guardar el usuario en una base de datos
+            user = User(email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+
+            # Devolver una respuesta JSON
+            response = {'message': 'Usuario creado exitosamente', 'email': email, 'password': password}
+            return jsonify(response), 201  # 201 significa "Created"
+        else:
+            return jsonify({'error': 'Los datos del usuario son inválidos'}), 400  # 400 significa "Bad Request"
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # 500 significa "Internal Server Error"
