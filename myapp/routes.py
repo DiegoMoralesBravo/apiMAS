ad56e1d9e0beb5d961ae5ca91d1b56efda2e5f1b
Diego Morales <diegomorales1359@gmail.com>
2023-10-18 19:28:56 -0600

    endpoint add added

--- myapp/routes.py
+++ myapp/routes.py
@@ -10,33 +10,24 @@ def index():
     users_list_html = [f"<li>{ user.email }</li>" for user in users]
     return f"<ul>{''.join(users_list_html)}</ul>"
 
-@main.route('/add/<username>')
-def add_user(username):
-    db.session.add(User(username=username, password='test'))
+@main.route('/add', methods=['POST'])
+def add_user():
+    # Get data from the request
+    data = request.json
+    print(data)
+    email = data.get('email')
+    occupation = data.get('occupation')
+    password = data.get('password')
+    confirmPassword = data.get('confirmPassword')
+
+    # Check if passwords match
+    if password != confirmPassword:
+        return jsonify({"message": "Passwords do not match"}), 400
+
+    # If validation passes, add the user to the database
+    user = User(username=email, password=password, occupation=occupation)
+    db.session.add(user)
     db.session.commit()
-    return redirect(url_for("main.index"))
 
-@main.route('/createUser', methods=['POST'])
-def create_user():
-    try:
-        user_data = request.get_json()  # Obtener el objeto JSON de la solicitud
-
-        # Verificar si se recibieron los datos esperados
-        if 'email' in user_data and 'password' in user_data:
-            email = user_data['email']
-            password = user_data['password']
-
-            # Realizar alguna lógica con los datos recibidos
-            # Por ejemplo, guardar el usuario en una base de datos
-            user = User(email=email, password=password)
-            db.session.add(user)
-            db.session.commit()
-
-            # Devolver una respuesta JSON
-            response = {'message': 'Usuario creado exitosamente', 'email': email, 'password': password}
-            return jsonify(response), 201  # 201 significa "Created"
-        else:
-            return jsonify({'error': 'Los datos del usuario son inválidos'}), 400  # 400 significa "Bad Request"
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500  # 500 significa "Internal Server Error"
+    # Return success response
+    return jsonify({"message": "User added successfully"}), 201
