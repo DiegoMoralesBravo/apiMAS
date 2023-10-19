c9d623635a9ad4a4cad621725c22c159846cb66f
Diego Morales <diegomorales1359@gmail.com>
2023-10-18 19:48:12 -0600

    User validation

--- myapp/routes.py
+++ myapp/routes.py
@@ -31,3 +31,26 @@ def add_user():
 
     # Return success response
     return jsonify({"message": "User added successfully"}), 201
+
+from werkzeug.security import check_password_hash
+from flask import request, jsonify
+
+@main.route('/userValidation', methods=['POST'])
+def userValidation():
+    # Get data from the request
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check if user with the given email exists
+    existing_user = User.query.filter_by(username=email).first()
+    
+    if not existing_user:
+        return jsonify({"message": "User does not exist"}), 404
+
+    # Check if the provided password matches the stored one
+    if not check_password_hash(existing_user.password, password):
+        return jsonify({"message": "Incorrect password"}), 401
+
+    # If validation passes, return success response
+    return jsonify({"message": "User validated successfully", "success": True,"user": {"email": existing_user.username, "occupation": existing_user.occupation}}), 200
