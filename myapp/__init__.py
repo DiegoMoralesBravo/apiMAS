0e7eb460547de1719aed0261654ba5f246043bc9
Diego Morales <diegomorales1359@gmail.com>
2023-10-18 19:55:39 -0600

    Cors

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,24 +8,24 @@ from flask_cors import CORS
 def create_app():
     
     app = Flask(__name__)
-    CORS(app)
+
+    # CORS setup
+    CORS(app, resources={r"/*": {"origins": "https://frontmastest.onrender.com"}})
+
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    # postgres://prettyprinted_render_example_user:11vq6k72GmFJazhVpz3pFUko50djVZT1@dpg-ceukdhmn6mpglqdb4avg-a.oregon-postgres.render.com/prettyprinted_render_example
 
     db.init_app(app)
     app.register_blueprint(main)
 
     # Check if the database needs to be initialized
     with app.app_context():
-        db.drop_all()
+        # Removed the drop_all() for safety reasons
         db.create_all()
         app.logger.info('Initialized the database!')
         
+        # Consider removing hardcoded user or ensuring it's only for testing
         new_user = User(email='diego@gmail.com', password='12345')
         db.session.add(new_user)
         db.session.commit()
-    
-    CORS(app, resources={r"/*": {"origins": "https://frontmastest.onrender.com"}})
-
         
     return app
